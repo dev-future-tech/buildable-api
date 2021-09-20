8eac7253f4132a94f006b787dac3d8854044afd2
Anthony Ikeda <anthony.ikeda@gmail.com>
2021-09-20 15:20:59 -0400

    Using hardcoded ip address for minio server

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    endpoint = "http://my-minio-service.default.svc.cluster.local:9000"
+    endpoint = "http://192.168.64.2:9000"
     key = "terraform.tfstate"
     region = "main"
     skip_requesting_account_id = true
