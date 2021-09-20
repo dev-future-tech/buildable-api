ba942d10dbaaedca33afe3d83ca71625c99c1c85
Anthony Ikeda <anthony.ikeda@gmail.com>
2021-09-20 15:58:22 -0400

    Correcting s3 config parameters

--- main.tf
+++ main.tf
@@ -6,9 +6,7 @@ terraform {
     access_key = "minio"
     secret_key = "miniostorage"
     bucket = "terraform"
-    skip_requesting_account_id = true
     skip_credentials_validation = true
-    skip_get_ec2_platforms = true
     skip_metadata_api_check = true
     skip_region_validation = true
     force_path_style = true
