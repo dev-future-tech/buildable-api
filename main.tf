4388b4f39d3127b29e2ed3d38ddf2560290c0082
Anthony Ikeda <anthony.ikeda@gmail.com>
2021-09-20 16:36:36 -0400

    Struggling with the kubernetes connection

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    endpoint = "http://my-minio-service:9000"
+    endpoint = "http://my-minio-service.default.svc:9000/"
     key = "terraform.tfstate"
     region = "main"
     access_key = "minio"
